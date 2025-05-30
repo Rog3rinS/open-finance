c65cd7c34cebb3589545a1dac4ea73e41dbb6ca6
Rog3rinS <rogeriogoioficial@gmail.com>
2025-05-30 10:08:14 -0300

    feat: created invoices migrations, also the models, the models are not done, it maybe need an update

---
+++ mini-banco-central/src/app/models/Invoices.js
@@ -0,0 +1,52 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Invoice extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+          unique: true,
+        },
+        account_id: {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+        },
+        status: {
+          type: Sequelize.ENUM('Vencida', 'Paga', 'Em aberto'),
+          allowNull: false,
+        },
+        amount: {
+          type: Sequelize.FLOAT,
+          allowNull: false,
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        due_date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        institution_id: {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        underscored: true,
+        tableName: 'invoices',
+        timestamps: true,
+      }
+    );
+
+    return this;
+    //ESSE CODIGO VAI PRECISAR FAZER O BELONGS TO PQ NAO TA FAZENDO NO INDEX.JS 
+  }
+};
+
+export default Invoice;
